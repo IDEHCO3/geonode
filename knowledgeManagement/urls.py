945b730a28fade0390114b87a709ae0cd796b4a2
indeco <indeco@gmail.com>
2014-12-16 17:34:09 -0200

    Added a little part, getMapKM and getLayerKM.

--- knowledgeManagement/urls.py
+++ knowledgeManagement/urls.py
@@ -5,8 +5,8 @@ urlpatterns = patterns('',
     #urls para listar conhecimentos armazenados
     url(r'^$', 'knowledgeManagement.views.index', name='index'),
     url(r'^(?P<knowledge_id>\d+)/$', 'knowledgeManagement.views.detail', name='detail'),
-    url(r'^(?P<map_id>\d+)/get_map_knowledge/$', 'knowledgeManagement.views.getMapKnowledge'),
-    url(r'^(?P<layer_id>\d+)/get_layer_knowledge/$', 'knowledgeManagement.views.getLayerKnowledge'),
+    url(r'^(?P<map_id>\d+)/get_map_knowledge/$', 'knowledgeManagement.views.getMapKnowledge', name='getMapKnowledge'),
+    url(r'^(?P<layer_id>\d+)/get_layer_knowledge/$', 'knowledgeManagement.views.getLayerKnowledge', name='getLayerKnowledge'),
     #urls para armazenar novos conhecimentos    
     url(r'^put_knowledge/$', 'knowledgeManagement.views.putKnowledge', name='inputKnowledge'),
     #url(r'^knowledgeManagement/(?P<layer_id>\d+)/put_layer_knowledge/$', 'knowledgeManagement.views.putLayerKnowledge'),
