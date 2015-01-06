5e87fc970735e38764b5869624175f5831fde002
kaitsuru <aluizio@dcc.ufrj.br>
2015-01-06 16:14:35 -0200

    Created main page of 'Layer editor' and 'new layer' page.

--- layerEditor/urls.py
+++ layerEditor/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('layerEditor.views',
-                       url(r'^$', 'index', name='index'))
+                       url(r'^$', TemplateView.as_view(template_name='layerEditor/index.html'), name='index'),
+                       url(r'^new_layer$', 'new_layer', name='new_layer'))
--- layerEditor/views.py
+++ layerEditor/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 # Create your views here.
-def index(request):
-    return render_to_response('layerEditor/index.html',
+def new_layer(request):
+    return render_to_response('layerEditor/new_layer.html',
         RequestContext(request, {}))
