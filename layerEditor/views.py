9427af07b87a61ced405b129050ea6406e2fa274
indeco <indeco@gmail.com>
2015-01-13 16:22:36 -0200

    The main part of layerEditor is done.

--- layerEditor/views.py
+++ layerEditor/views.py
@@ -4,6 +4,10 @@ from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 import shapefile as sf
+from subprocess import call
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Create your views here.
 def new_layer(request):
@@ -13,7 +17,9 @@ def new_layer(request):
 def create_layer(request):
 
     marker_type = {'Point': sf.POINT, 'Line': sf.POLYLINE, 'Polygon': sf.POLYGON}
-    samples_elements = {'Point': [[[-22,-33]]], 'Line': [[[-22,-33],[-21,-32]]], 'Polygon': [[[-22,-33],[-21,-32],[-22,-32],[-22,-33]]]}
+    samples_elements = {'Point': [[[-43.2096, -22.9035]]],
+                        'Line': [[[-43.2096, -22.9035], [-43.1996, -22.8935]]],
+                        'Polygon': [[[-43.2096, -22.9035], [-43.1996, -22.8935], [-43.2096, -22.8935], [-43.2096, -22.9035]]]}
 
     attributes = request.POST
     layer_name = request.POST["layer_name"]
@@ -35,7 +41,7 @@ def create_layer(request):
 
     user_shape.poly(parts=samples_elements[layer_type])
     user_shape.record(id='0')
-    path_file = '/home/indeco/geonode/downloaded/'+layer_name
+    path_file = PROJECT_ROOT+'/temp_shapes/'+layer_name
 
     prj = open("%s.prj" % path_file, "w")
     epsg = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]'
@@ -44,4 +50,15 @@ def create_layer(request):
 
     user_shape.save(path_file)
 
-    return HttpResponseRedirect(reverse('maploom-map-new'))
+    command_add_layer = "python manage.py importlayers "
+    #command_add_layer += "-u aluizio "
+    command_add_layer += path_file + ".shp"
+    call(command_add_layer, shell=True)
+
+    command_remove_shape = "rm -f "+path_file+".*"
+    call(command_remove_shape, shell=True)
+
+    arguments = "?layer=geonode:"+layer_name
+    arguments = arguments.encode("latin_1")
+
+    return HttpResponseRedirect(reverse('maploom-map-new') + arguments )
