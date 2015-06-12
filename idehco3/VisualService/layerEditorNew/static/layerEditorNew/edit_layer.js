fa5b16aaf775da3914296e1c35a2bcec41209b8f
Kaitsuru <aluizio@dcc.ufrj.br>
2015-06-12 16:50:42 -0300

    Created the new js to manager the layer in editLayerNew.

--- idehco3/VisualService/layerEditorNew/static/layerEditorNew/edit_layer.js
+++ idehco3/VisualService/layerEditorNew/static/layerEditorNew/edit_layer.js
@@ -1,9 +1,7 @@
 /**
  * Created by indeco on 5/28/15.
  */
-var raster = new ol.layer.Tile({
-  source: new ol.source.MapQuest({layer: 'sat'})
-});
+
 
 var  data_geo = document.getElementById("data_geo");
 data_geo = JSON.parse(data_geo.innerText.toString());
@@ -20,27 +18,15 @@ var geojsonObject = {
     {
       'type': 'Feature',
       'geometry': data_geo
-    }]
-};
+    }
+]};
 
-var b = new ol.source.GeoJSON({'object': geojsonObject, projection: 'EPSG:3857'});
-var source2 = new ol.layer.Vector({source : b});
+var source2 = new ol.source.GeoJSON({'object': geojsonObject, 'projection': 'EPSG:3857'});
 
-var map = new ol.Map({
-  layers: [raster, source2],
-  target: 'map',
-  view: new ol.View({
-    projection: 'EPSG:3857',
-    center: [0, 0],
-    zoom: 2
-  })
-});
-
-// The features are not added to a regular vector layer/source,
-// but to a feature overlay which holds a collection of features.
-// This collection is passed to the modify and also the draw
-// interaction, so that both can add or modify features.
-var featureOverlay = new ol.FeatureOverlay({
+// create a vector layer used for editing
+var vector_layer = new ol.layer.Vector({
+  name: 'my_vectorlayer',
+  source: source2,
   style: new ol.style.Style({
     fill: new ol.style.Fill({
       color: 'rgba(255, 255, 255, 0.2)'
@@ -57,50 +43,210 @@ var featureOverlay = new ol.FeatureOverlay({
     })
   })
 });
-featureOverlay.setMap(map);
-featureOverlay.setFeatures(new ol.Collection(source2.getSource().getFeatures()));
 
-var modify = new ol.interaction.Modify({
-  features: featureOverlay.getFeatures(),
-  // the SHIFT key must be pressed to delete vertices, so
-  // that new vertices can be drawn at the same position
-  // of existing vertices
-  deleteCondition: function(event) {
-    return ol.events.condition.shiftKeyOnly(event) &&
-        ol.events.condition.singleClick(event);
+// Create a map
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.MapQuest({layer: 'sat'})
+    }),
+    vector_layer
+  ],
+  view: new ol.View({
+    zoom: 2,
+    center: [0, 0],
+    projection: 'EPSG:3857'
+  })
+});
+
+// make interactions global so they can later be removed
+var select_interaction,
+    draw_interaction,
+    modify_interaction;
+
+// get the interaction type
+var $interaction_type = $('[name="interaction_type"]');
+// rebuild interaction when changed
+$interaction_type.on('click', function(e) {
+  // add new interaction
+  if (this.value === 'draw') {
+    addDrawInteraction();
+  } else {
+    addModifyInteraction();
   }
 });
-map.addInteraction(modify);
 
-var draw; // global so we can remove it later
-function addInteraction() {
-  draw = new ol.interaction.Draw({
-    features: featureOverlay.getFeatures(),
-    type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
-  });
-  map.addInteraction(draw);
-}
+// get geometry type
+var $geom_type = $('#geom_type');
+// rebuild interaction when the geometry type is changed
+$geom_type.on('change', function(e) {
+  map.removeInteraction(draw_interaction);
+  addDrawInteraction();
+});
 
-var typeSelect = document.getElementById('type');
-
-
-/**
- * Let user change the geometry type.
- * @param {Event} e Change event.
- */
-typeSelect.onchange = function(e) {
-  map.removeInteraction(draw);
-  addInteraction();
+// get data type to save in
+$data_type = $('#data_type');
+// clear map and rebuild interaction when changed
+$data_type.onchange = function() {
+  clearMap();
+  map.removeInteraction(draw_interaction);
+  addDrawInteraction();
 };
 
-addInteraction();
+// build up modify interaction
+// needs a select and a modify interaction working together
+function addModifyInteraction() {
+  // remove draw interaction
+  map.removeInteraction(draw_interaction);
+  // create select interaction
+  select_interaction = new ol.interaction.Select({
+    // make sure only the desired layer can be selected
+    layers: function(vector_layer) {
+      return vector_layer.get('name') === 'my_vectorlayer';
+    }
+  });
+  map.addInteraction(select_interaction);
 
-var save = function(){
-  var format = new ol.format.GeoJSON();
-  var features = map.getInteractions().getFeature();
-  var json = format.writeFeatures(features);
-  var text = JSON.stringify(json);
-  console.log(text);
+  // grab the features from the select interaction to use in the modify interaction
+  var selected_features = select_interaction.getFeatures();
+  // when a feature is selected...
+  selected_features.on('add', function(event) {
+    // grab the feature
+    var feature = event.element;
+    // ...listen for changes and save them
+    feature.on('change', saveData);
+    // listen to pressing of delete key, then delete selected features
+    $(document).on('keyup', function(event) {
+      if (event.keyCode == 46) {
+        // remove all selected features from select_interaction and my_vectorlayer
+        selected_features.forEach(function(selected_feature) {
+          var selected_feature_id = selected_feature.getId();
+          // remove from select_interaction
+          selected_features.remove(selected_feature);
+          // features aus vectorlayer entfernen
+          var vectorlayer_features = vector_layer.getSource().getFeatures();
+          vectorlayer_features.forEach(function(source_feature) {
+            var source_feature_id = source_feature.getId();
+            if (source_feature_id === selected_feature_id) {
+              // remove from my_vectorlayer
+              vector_layer.getSource().removeFeature(source_feature);
+              // save the changed data
+              saveData();
+            }
+          });
+        });
+        // remove listener
+        $(document).off('keyup');
+      }
+    });
+  });
+  // create the modify interaction
+  modify_interaction = new ol.interaction.Modify({
+    features: selected_features,
+    // delete vertices by pressing the SHIFT key
+    deleteCondition: function(event) {
+      return ol.events.condition.shiftKeyOnly(event) &&
+        ol.events.condition.singleClick(event);
+    }
+  });
+  // add it to the map
+  map.addInteraction(modify_interaction);
 }
 
-$("#writeLayer").on('click', save);
+// creates a draw interaction
+function addDrawInteraction() {
+  // remove other interactions
+  map.removeInteraction(select_interaction);
+  map.removeInteraction(modify_interaction);
+
+  // create the interaction
+  draw_interaction = new ol.interaction.Draw({
+    source: vector_layer.getSource(),
+    type: /** @type {ol.geom.GeometryType} */ ($geom_type.val())
+  });
+	// add it to the map
+  map.addInteraction(draw_interaction);
+
+  // when a new feature has been drawn...
+  draw_interaction.on('drawend', function(event) {
+    // create a unique id
+    // it is later needed to delete features
+    var id = uid();
+    // give the feature this id
+    event.feature.setId(id);
+    // save the changed data
+    saveData();
+  });
+}
+
+// add the draw interaction when the page is first shown
+addDrawInteraction();
+
+// shows data in textarea
+// replace this function by what you need
+function saveData() {
+  // get the format the user has chosen
+  var data_type = $data_type.val(),
+      // define a format the data shall be converted to
+ 		format = new ol.format[data_type](),
+      // this will be the data in the chosen format
+ 		data;
+  try {
+    // convert the data of the vector_layer into the chosen format
+    data = format.writeFeatures(vector_layer.getSource().getFeatures());
+  } catch (e) {
+    // at time of creation there is an error in the GPX format (18.7.2014)
+    $('#data').val(e.name + ": " + e.message);
+    return;
+  }
+  if ($data_type.val() === 'GeoJSON') {
+    // format is JSON
+    $('#data').val(JSON.stringify(data, null, 4));
+  } else {
+    // format is XML (GPX or KML)
+    var serializer = new XMLSerializer();
+    $('#data').val(serializer.serializeToString(data));
+  }
+}
+
+// clear map when user clicks on 'Delete all features'
+$("#delete").click(function() {
+  clearMap();
+});
+
+// clears the map and the output of the data
+function clearMap() {
+  vector_layer.getSource().clear();
+  if (select_interaction) {
+  	select_interaction.getFeatures().clear();
+  }
+  $('#data').val('');
+}
+
+// creates unique id's
+function uid(){
+  var id = 0;
+  return function() {
+    if (arguments[0] === 0) {
+      id = 0;
+    }
+    return id++;
+  }
+}
+
+var save = function()
+{
+    var source = vector_layer.getSource();
+    var features = source.getFeatures();
+    var format = new ol.format.GeoJSON();
+    var json = format.writeFeatures(features);
+
+    var source_4326 = new ol.source.GeoJSON({'object': json, 'defaultProjection': "EPSG:3857", 'projection': "EPSG:4326"});
+    var json_4326 = format.writeFeatures(source_4326.getFeatures());
+    console.log(json_4326);
+
+
+    var text = JSON.stringify(json_4326);
+    console.log(text);
+}
